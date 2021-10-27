7640363bbf2e21ec09fa5bdcca60997b5b923ec5
Borzik Konstantin <kosty5192@gmail.com>
2021-10-28 00:31:35 +0300

    Changes

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -14,15 +14,19 @@ import com.google.api.services.sheets.v4.SheetsScopes;
 import com.google.api.services.sheets.v4.model.ValueRange;
 
 import java.io.*;
+import java.lang.reflect.Array;
 import java.security.GeneralSecurityException;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 public class Main {
     private static Sheets sheetsService;
     private static String APPLICATION_NAME = "MY GOOGLE SHEETS Project";
     private static String SPREADSHEET_ID = "1p3gh90EYeDzQNe4rjf8JmMN5sbOEsAWj6c7a1UjnKR8";
+    static List<List<Object>> values ;
 
     private static Credential authorize() throws IOException,GeneralSecurityException{
 
@@ -54,16 +58,79 @@ public class Main {
                 .build();
     }
 
-    public static void main(String[] args) throws IOException,GeneralSecurityException{
-        sheetsService = getSheetsService();
-        String range = "mainList!A2:D";
+    public static synchronized  List<List<Object>> getDataFromSheets() throws IOException {
+        try {
+            sheetsService = getSheetsService();
+        } catch (IOException e) {
+            System.out.println(e);
+        } catch (GeneralSecurityException e) {
+            System.out.println(e);
+        }
+        String range = "mainList!A2:F";
 
         ValueRange response= sheetsService.spreadsheets().values()
                 .get(SPREADSHEET_ID,range)
                 .execute();
         List<List<Object>> values  = response.getValues();
-        PdfCreator creator = new PdfCreator(values);
-        creator.Start();
+        for (List<Object> el : values){
+            for(Object a : el){
+                if(a.toString() == null){
+                    values.remove(el);
+                    break;
+                }
+            }
+        }
+        return values;
+    }
+
+    public static synchronized  List<List<Object>> getNeededEltoCreatePDF( List<List<Object>> data) throws FileNotFoundException {
+        ArrayList<List<Object>> neededData = new ArrayList<List<Object>>();
+        File file = new File("lastStudentCode.txt");
+        //FileReader reader = new FileReader(file);
+        //String str = reader.;
+        String lastCode = PdfCreator.getLastStudentCode();
+        if(lastCode == null || lastCode.isEmpty()){
+            neededData.addAll(data);
+        }else{
+            long lastCodeLong = Long.parseLong(lastCode);
+            for (List<Object> el : data){
+                String codeEl =  el.get(2).toString();
+                long codeElLong = Long.parseLong(codeEl);
+                if(lastCodeLong < codeElLong){
+                    neededData.add(el);
+                }
+            }
+        }
+        return  neededData;
+    }
+
+    public static void main(String[] args) throws IOException, GeneralSecurityException, InterruptedException {
+        Object lock = new Object();
+
+        Runnable task = () -> {
+                try {
+                    values = getDataFromSheets();
+                    List<List<Object>> nonCertificated = getNeededEltoCreatePDF(values);
+                    if(!nonCertificated.isEmpty()){
+                        PdfCreator creator = new PdfCreator(nonCertificated);
+                        creator.start();
+                    }
+                } catch (IOException e) {
+
+                }
+        };
+        boolean stopButton = false;
+        while (!stopButton){
+            Thread t = new Thread(task);
+            t.start();
+            //System.out.println("Chose operation:");
+
+
+
+            TimeUnit.SECONDS.sleep(60);
+        }
+
+
     }
 
 }
--- src/main/java/PdfCreator.java
+++ src/main/java/PdfCreator.java
@@ -2,111 +2,76 @@ import com.itextpdf.io.image.ImageData;
 import com.itextpdf.io.image.ImageDataFactory;
 import com.itextpdf.kernel.pdf.PdfWriter;
 import com.itextpdf.layout.element.Image;
-import com.sun.org.apache.bcel.internal.generic.INEG;
-import jdk.javadoc.internal.doclets.toolkit.DocletException;
-import org.apache.pdfbox.cos.COSBase;
-import org.apache.pdfbox.cos.COSObject;
-import org.apache.pdfbox.cos.ICOSVisitor;
-import org.apache.pdfbox.exceptions.COSVisitorException;
-import org.apache.pdfbox.pdmodel.PDDocument;
-import org.apache.pdfbox.pdmodel.PDPage;
-import org.apache.pdfbox.pdmodel.edit.PDPageContentStream;
-import org.apache.pdfbox.pdmodel.graphics.xobject.PDPixelMap;
-import  org.apache.pdfbox.pdmodel.graphics.xobject.PDXObjectImage;
 
 import com.itextpdf.kernel.pdf.*;
 import com.itextpdf.layout.Document;
 import com.itextpdf.layout.element.Paragraph;
-import sun.jvm.hotspot.memory.FileMapInfo;
 
-import javax.imageio.stream.*;
-import javax.imageio.ImageIO;
-import java.awt.image.BufferedImage;
 import java.io.*;
-import java.net.URISyntaxException;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.List;
-import java.util.Scanner;
 
 public class PdfCreator {
     private List<List<Object>> data;
+    private static String lastStudentCode;
+    private  static  String certificateBgPath = "C:\\Users\\Serpuzalot\\Desktop\\JavaStart\\JavaProject\\src\\main\\resources\\certificatePhoto.jpg";
 
     PdfCreator(List<List<Object>> data){
         this.data = data;
     }
 
-    public void  Start(){
-        printStudents();
-        int studentIndex = choiceStudent();
-        createPdf(studentIndex);
+    public static String getLastStudentCode() {
+        return lastStudentCode;
     }
 
-    private  int choiceStudent(){
-        Scanner scan = new Scanner(System.in);
-        System.out.println("Choice a student who need certificate: ");
-        String choiceStr = scan.nextLine();
-        boolean checkError = tryParseInt(choiceStr) ;
-        int chocie =  checkError ? Integer.parseInt(choiceStr) - 1 : 0 ;
-        while (!checkError || chocie < 0 || chocie > data.size() ){
-            System.out.println("Input error,try again");
-            System.out.println("Choice a student who need certificate: ");
-            choiceStr = scan.nextLine();
-            checkError = tryParseInt(choiceStr);
-            chocie =  checkError ? Integer.parseInt(choiceStr) - 1 : 0 ;
-        }
-        return chocie;
+    public static String getSertificateBgImage() {
+        return certificateBgPath;
     }
 
-    private boolean tryParseInt(String str){
-        try {
-            Integer.parseInt(str);
-            return true;
-        }catch (NumberFormatException e){
-            return  false;
-        }
+    public static void setSertificateBgImage(String sertificateBgImage) {
+        PdfCreator.certificateBgPath = sertificateBgImage;
     }
 
-    private void printStudents(){
-        if(data.isEmpty() || data == null) {
-            System.out.println("No data found!");
-            return;
+    public void  start(){
+        for (List<Object> student : data){
+            createPdf(student);
         }
-        for (List row : data ) {
-            System.out.print(data.indexOf(row)+1);
-            System.out.printf(":  %s %s code: %s phone: %s\n",row.get(0),row.get(1),row.get(2),row.get(3));
-        }
+
     }
 
-    private void createPdf(int studentIndex)  {
-        String certificateBgPath = getBackgroundFilePath();
-
+    private void createPdf(List<Object> student)  {
         try{
-            String resultPath = "/Result";
-            PdfDocument pdf = new PdfDocument(new PdfWriter("Cetrificate for " + data.get(studentIndex).get(0).toString()));
+            String resultPath = ".\\Results\\Cetrificate For " + student.get(0) + student.get(1) +".pdf";
+            File result = new File(resultPath);
+            PdfDocument pdf = new PdfDocument((new PdfWriter(resultPath)));
+            pdf.addNewPage();
             Document document = new Document(pdf);
-            String line = data.get(studentIndex).get(0).toString() + " " +data.get(studentIndex).get(1).toString();
-            ImageData data = ImageDataFactory.create(certificateBgPath);
-            Image img = new Image(data);
-            document.add(img);
-            document.add(new Paragraph(line).setFixedPosition(240,590,1000).setFontSize(24));
+            String line = student.get(0).toString() + " " + student.get(1).toString() ;
+            ImageData imageData = ImageDataFactory.create(certificateBgPath);
+            Image img = new Image(imageData);
+            document.add(img.setFixedPosition(0,0).setWidth(595.276f).setHeight(841.89f));
+            document.add(new Paragraph(line).setFixedPosition(230,490,1000).setFontSize(24));
+            document.add(new Paragraph("Subject:").setFixedPosition(235,460,1000).setFontSize(24));
+            line = student.get(4).toString();
+            document.add(new Paragraph(line).setFixedPosition(230,430,1000).setFontSize(24));
+            line = "Signature:  ______";
+            document.add(new Paragraph(line).setFixedPosition(300,150,400).setFontSize(16));
+            line = "Odessa National Polytechnic University";
+            document.add(new Paragraph(line).setFixedPosition(200,120,400).setFontSize(14));
             document.close();
+            lastStudentCode =  student.get(2).toString();
+            saveLastStudentCode(lastStudentCode);
         }catch (IOException e){
 
         }
     }
+    private void saveLastStudentCode (String code) throws IOException {
+        File file = new File("lastStudentCode.txt");
+        FileWriter writer = new FileWriter(file);
+        writer.write(code);
+        writer.close();
 
-    private String getBackgroundFilePath(){
-        System.out.println("Input file path to background : ");
-        Scanner scan = new Scanner(System.in);
-        String path = scan.nextLine();
-        File fileInfo = new File(path);
-        while (!fileInfo.exists()){
-            System.out.println("Input file path error,try again");
-            path = scan.nextLine();
-            fileInfo = new File(path);
-        }
-        return  path;
     }
 
+
+ //TODO: fix position + add to googleTable last cod student add work gdrive cloud start programm + doc
 }
